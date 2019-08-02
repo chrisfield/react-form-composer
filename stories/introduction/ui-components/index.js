f858a8cbde73675baf87a21b4babeb7b9bcbf23e
chrisfield <chris@christopherfield.co.uk>
2019-08-02 06:12:19 +0100

    Improve docs

--- stories/introduction/ui-components/index.js
+++ stories/introduction/ui-components/index.js
@@ -3,7 +3,7 @@ import readme from './index.md'
 
 import React from 'react';
 import {FormStateProvider, Form, useForm, useFormReducer} from '../../../packages/react-form-composer/src';
-import {TextInput, NumberInput, Select, Checkbox, RadioButton} from '../../ui-components';
+import { NumberInput } from './number-input';
 
 const TheFormState = () => {
   const [state] = useFormReducer(useForm().name);
@@ -27,30 +27,7 @@ const MyForm = () => {
       <Form name="myForm" onSubmit={submitValues} onSubmitSuccess={clearValues}>
         <div style={{ display: 'flex', flexWrap: 'wrap' }}>
           <div style={{ flex: 1, marginRight: '2rem' }}>
-            <div>
-              <TextInput name="fieldOne" label="Field One" required/>
-              <NumberInput name="age" required label="Age"/>
-              <Select label="Frequency" name="frequency" required>
-                <option value="" disabled>
-                  Select One...
-                </option>
-                <option value="daily">Daily</option>
-                <option value="weekly">Weekly</option>
-                <option value="monthly">Monthly</option>
-              </Select>
-              <Select label="Exercise" name="exercice" multiple>
-                <option value="Walk">walk</option>
-                <option value="Run">run</option>
-                <option value="Cycle">cycle</option>
-                <option value="Swim">swim</option>
-              </Select>
-              <Checkbox name="isAgreed" label="Do you agree?"/>
-            </div>
-            <div>
-              <RadioButton name="rb2" label="Red" value="R" />
-              <RadioButton name="rb2" label="Green" value="G" />
-              <RadioButton selected name="rb2" label="Blue" value="B" />
-            </div>
+            <NumberInput name="age" required label="Age"/>
             <Button/>
           </div>
           <div style={{
--- stories/introduction/ui-components/number-input.jsx
+++ stories/introduction/ui-components/number-input.jsx
@@ -1,22 +1,6 @@
-# Writing ui-Components
+import React from 'react';
+import {Field} from '../../../packages/react-form-composer/src';
 
-The built-in ui-components make it easy to get started when you want to write forms with the standard html controls. These built-in components are written using the react-form-composer `Field` api so you might want to copy and adapt their code when writing your own controls:
-* [Text](https://github.com/chrisfield/react-form-composer/blob/master/packages/react-form-composer/src/ui-components/text.js)
-* [TextArea](https://github.com/chrisfield/react-form-composer/blob/master/packages/react-form-composer/src/ui-components/text-area.js)
-* [RadioGroup and Radio](https://github.com/chrisfield/react-form-composer/blob/master/packages/react-form-composer/src/ui-components/radio.js)
-* [Checkbox](https://github.com/chrisfield/react-form-composer/blob/master/packages/react-form-composer/src/ui-components/checkbox.js)
-* [Select](https://github.com/chrisfield/react-form-composer/blob/master/packages/react-form-composer/src/ui-components/select.js)
-* [ValidationMessage](https://github.com/chrisfield/react-form-composer/blob/master/packages/react-form-composer/src/ui-components/validation-message.js)
-
-
-## Example custom control: NumberInput
-The form below includes a custom `NumberInput` that acepts integers and shows the value formatted to the locale eg with commas separating in UK (1,000,000).
-
-<!-- STORY -->
-
----
-#### Code
-```jsx
 const InputWrapper = ({label, id, name, touched, error, children}) => (
   <div>
     <label htmlFor={id + name}>{label || name}</label>
@@ -111,12 +95,3 @@ export const NumberInput = ({required, ...props}) => {
 };
 
 export default NumberInput;
-```
----
-
-#### Notes
-
-The functions for `formatFromStore` and `formatToStore` are fairly self explaintary. 
-
-Any function you pass as a `beforeUpdate` prop will be called with the field, the old value and the new value. Whatever your `beforeUpdate` function returns will be passed to any `afterUpdate` as the 2nd param (the 1st param being the field). In the code below I use `beforeUpdate` and `afterUpdate` to keep the cursor position.
-
